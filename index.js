5bc9d45b5c40032747e451760a08ee4cfa11d6f8
priegelsdev <priegelsdev@gmail.com>
2022-11-19 22:30:03 +0100

    set up getFeedHtml fn + render fn

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 import {ticketData} from "/data.js";
 
-console.log(ticketData);
+//create function to get item feed from data.js
 
+function getFeedHtml() {
+  console.log(ticketData);
+
+  let itemFeedHtml = '';
+
+  ticketData.forEach(ticket => {
+
+    itemFeedHtml += `
+      <div class="item-container">
+        <img src="https://via.placeholder.com/150" class="item-img">
+      
+        <div class="text-container">
+          <h3 class="item-name">${ticket.name}</h3>
+          <p class="item-desc">${ticket.description}</p>
+          <h4 class="item-price">$${ticket.price}</h4>
+        </div>
+      
+        <button class="add-btn">+</button>
+      </div>
+      ` 
+  })
+
+  return itemFeedHtml;
+
+
+}
+
+// create render function
+
+function render() {
+  document.querySelector('.item-feed').innerHTML = getFeedHtml();
+}
+
+// call render
+
+render();
 
 
 
